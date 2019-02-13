21185901b11da6f82963218dcc978a591f09f6e3
ElizaHlglh <ewu4@stuy.edu>
2019-02-13 11:39:39 -0500

    KnightBoard.java

---
+++ KnightBoard.java
@@ -0,0 +1,67 @@
+public class KnightBoard{
+  private int[][] board;
+  //@throws IllegalArgumentException when either parameter is negative.
+  public KnightBoard(int startingRows,int startingCols){
+    try{
+      int[][] board = new int[startingRows][startingCols];
+      for (int row = 0; row < board.length; row++){
+        for (int col = 0; col < board[row].length; col++){
+          board[row][col] = 0;
+        }
+      }
+    }
+    catch(IllegalArgumentException e){
+      System.out.println("CAN'T HAVE NEGATIVE BOARD SIZE");
+    }
+  }
+
+      //Initialize the board to the correct size and make them all 0's
+
+
+  public String toString(){
+    String ans = "";
+    if (board.length * board[0].length >= 10){
+      for (int row = 0; row < board.length; row++){
+        for (int col = 0; col < board[row].length; col++){
+          if (board[row][col] < 10){
+            ans += " " + board[row][col] + " ";
+          }
+          else{
+            ans += board[row][col] + " ";
+          }
+        }
+        ans += "\n";
+      }
+    }
+    else{
+      for (int row = 0; row < board.length; row++){
+        for (int col = 0; col < board[row].length; col++){
+          ans += board[row][col] + " ";
+        }
+        ans += "\n";
+      }
+    }
+    return ans;
+  }
+
+/*  @throws IllegalStateException when the board contains non-zero values.
+  @throws IllegalArgumentException when either parameter is negative
+   or out of bounds.*/
+  public boolean solve(int startingRow, int startingCol){
+
+  }
+
+/*  @throws IllegalStateException when the board contains non-zero values.
+  @throws IllegalArgumentException when either parameter is negative
+   or out of bounds.*/
+  public int countSolutions(int startingRow, int startingCol){
+
+  }
+
+  //Suggestion:
+  private boolean solveH(int row ,int col, int level){
+
+  }
+  // level is the # of the knight
+
+}
